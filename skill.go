2f2f1a3d609d9d90a47a49386fb6aaa8b086f6e7
LABOUARDY Mohamed <mohamed.labouardy@gmail.com>
2018-03-29 20:13:42 +0200

    Update skill.go

--- skill.go
+++ skill.go
@@ -3,61 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type AlexaRequest struct {
-	Version string `json:"version"`
-	Session struct {
-		Attributes map[string]interface{} `json:"attributes"`
-	} `json:"session"`
-	Request struct {
-		Type   string `json:"type"`
-		Time   string `json:"timestamp"`
-		Intent struct {
-			Name  string          `json:"name"`
-			Slots map[string]Slot `json:"slots"`
-		} `json:"intent"`
-	} `json:"request"`
-}
-
-type Slot struct {
-	Name  string `json:"name"`
-	Value string `json:"value"`
-}
-
-type AlexaResponse struct {
-	Version           string                 `json:"version"`
-	SessionAttributes map[string]interface{} `json:"sessionAttributes"`
-	Response          struct {
-		ShouldEndSession bool `json:"shouldEndSession"`
-		OutputSpeech     struct {
-			Type string `json:"type"`
-			Text string `json:"text"`
-		} `json:"outputSpeech"`
-	} `json:"response"`
-}
-
-func CreateResponse() *AlexaResponse {
-	var resp AlexaResponse
-	resp.Version = "1.0"
-	resp.Response.OutputSpeech.Type = "PlainText"
-	resp.Response.OutputSpeech.Text = "Hello.  Please override this default output."
-	return &resp
-}
-
-func (resp *AlexaResponse) Say(text string, endSession bool) {
-	resp.Response.OutputSpeech.Text = text
-	resp.Response.ShouldEndSession = endSession
-}
+const welcomeMessage = "Welcome to containers world. Say 'Deploy a new Swarm cluster' to begin"
+const exitMessage = "See you later till then may the containers be with you"
 
 func HandleRequest(ctx context.Context, r AlexaRequest) (AlexaResponse, error) {
-	log.Println("Request:")
-	log.Println(r)
-	log.Printf("Request type is: ", r.Request.Intent.Name)
-
 	resp := CreateResponse()
 
 	switch r.Request.Intent.Name {
@@ -65,14 +18,20 @@ func HandleRequest(ctx context.Context, r AlexaRequest) (AlexaResponse, error) {
 		resp.Say("How many nodes do you want?", false)
 	case "ClusterSize":
 		resp.SessionAttributes = make(map[string]interface{}, 1)
-		resp.SessionAttributes["clusterSize"] = r.Request.Intent.Slots["size"]
+		resp.SessionAttributes["clusterSize"] = r.Request.Intent.Slots["size"].Value
 		resp.Say("What do you want to name your cluster?", false)
 	case "ClusterName":
-		resp.Say(fmt.Sprintf("Thanks for selecting the size %v", r.Session.Attributes["clusterSize"]), true)
+		clusterSize := r.Session.Attributes["clusterSize"]
+		clusterName := r.Request.Intent.Slots["cluster"].Value
+		resp.Say(fmt.Sprintf("Sure, your %v cluster with %v nodes is being created", clusterName, clusterSize), true)
 	case "AMAZON.HelpIntent":
-		resp.Say("Welcome to containers world. Say 'Deploy a new Swarm cluster' to begin", false)
+		resp.Say(welcomeMessage, false)
+	case "AMAZON.StopIntent":
+		resp.Say(exitMessage, true)
+	case "AMAZON.CancelIntent":
+		resp.Say(exitMessage, true)
 	default:
-		resp.Say("Welcome to containers world. Say 'Deploy a new Swarm cluster' to begin", false)
+		resp.Say(welcomeMessage, false)
 	}
 	return *resp, nil
 }
